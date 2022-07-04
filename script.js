20773e0951858d2dc6a77852d8bd16c37334bda3
baraka <timothybaraka44@gmail.com>
2022-07-04 12:52:23 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var button = document.getElementById("btn");
+/* button.addEventListener("click",function(){
+   alert("You just clicked me");
+ });*/
+// button.onclick = function () {
+//    window.location.assign("/")
+// }
+
